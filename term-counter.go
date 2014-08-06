2d0cc8d18543c12aab557a249cd0c2036ae0b630
Kang-min Liu <gugod@gugod.org>
2014-08-06 10:46:36 +0200

    use sync to avoid data race

--- term-counter.go
+++ term-counter.go
@@ -3,22 +3,34 @@ package main
 
 import (
 	"strings"
+	"sync"
 	"net/http"
 	"encoding/json"
 )
 
-var termcount map[string]int;
+var (
+	termcount map[string]int
+	termcountMu sync.Mutex
+)
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
+		var wg sync.WaitGroup
 		tc := make( map[string]int )
 
 		r.ParseForm();
 		arr := strings.Split( r.Form.Get("q"), " " )
+		wg.Add( len(arr) )
 		for i := 0; i < len(arr); i++ {
-			termcount[ arr[i] ]++
-			tc[arr[i]] = termcount[ arr[i] ]
+			go func(term string) {
+				termcountMu.Lock()
+				defer termcountMu.Unlock()
+				termcount[ term ]++
+				tc[term] = termcount[ term ]
+				wg.Done()
+			}(arr[i]);
 		}
+		wg.Wait()
 
 		b, err := json.Marshal(tc)
 		if err == nil {
@@ -27,6 +39,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == "GET" {
+		termcountMu.Lock()
+		defer termcountMu.Unlock()
+
 		b, err := json.Marshal(termcount)
 		if err == nil {
 			w.Write(b)
