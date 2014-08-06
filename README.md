d0c1f5d68a1782640586768ac7f853cebcd12379
Kang-min Liu <gugod@gugod.org>
2014-08-06 08:37:10 +0200

    json response

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 term-counter-go
 ===============
+
+Run:
+
+    go run term-counter.go
+
+Send query string with terms...
+
+    curl -XPOST http://localhost:3000/'?q=foo+bar+baz'
+
+Retrieve term counts.
+
+    curl http://localhost:3000/
+
--- term-counter.go
+++ term-counter.go
@@ -2,21 +2,35 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"strings"
+	"net/http"
+	"encoding/json"
 )
 
 var termcount map[string]int;
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
+		tc := make( map[string]int )
+
 		r.ParseForm();
 		arr := strings.Split( r.Form.Get("q"), " " )
 		for i := 0; i < len(arr); i++ {
-			termcount[ arr[i] ]++;
-			fmt.Fprintf(w, "%s = %d\n", arr[i], termcount[arr[i]])
+			termcount[ arr[i] ]++
+			tc[arr[i]] = termcount[ arr[i] ]
 		}
+
+		b, err := json.Marshal(tc)
+		if err == nil {
+			w.Write(b)
+		} 
+	}
+
+	if r.Method == "GET" {
+		b, err := json.Marshal(termcount)
+		if err == nil {
+			w.Write(b)
+		} 
 	}
 }
 
